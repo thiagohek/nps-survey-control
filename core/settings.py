4a7626032a04014d2363a4f2bf2d739d6eaa1052
Thiago Karnopp <thiagokarnopp@MacBook-Air-de-Thiago.local>
2026-03-31 10:52:08 -0300

    Add debug prints

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,11 @@ from decouple import config
 import dj_database_url
 
 
+import os
+print("SECRET_KEY exists:", "SECRET_KEY" in os.environ)
+print("All env vars:", list(os.environ.keys()))
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
