07cd2e372a06a8c9929b403e1518e372bc3de6ac
Thiago Karnopp <thiagokarnopp@MacBook-Air-de-Thiago.local>
2026-03-31 15:00:57 -0300

    Update login

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["https://web-production-21d9.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS= ["https://web-production-21d9.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
